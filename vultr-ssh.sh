578cf9bed902049c6c210f6959cc3c5be16c8dad
Enrique Moron Ayuso <emoronayuso@gmail.com>
2015-05-27 12:36:29 +0200

    Se incluyen mejoras al script
    Entre las mejoras esta la consulta local de resolucion de dns desde el
    fichero /etc/hosts, ya que el comando 'vucli' con la opcion 'serverlist' te devuelve la ip de cada maquina y no el nombre de dominio. Tambien se incluye la posibilidad de elegir el usuario de la conexion ssh
    
    Signed-off-by: Enrique Moron Ayuso <emoronayuso@gmail.com>

--- vultr-ssh.sh
+++ vultr-ssh.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 # Require vultr-cli https://github.com/yikaus/vultrcli
+# NOTE: Resolve IPs from Local DNS from /etc/hosts (comand host)
 port_ssh="22" # Port ssh 
 api_key="" # Api key vultr
 clear
-/usr/local/bin/vucli.py serverlist -k $api_key|awk '{print $2}'|grep -v label|sort > /tmp/vpsvultr
-cat -b /tmp/vpsvultr
+#/usr/local/bin/vucli.py serverlist -k $api_key|awk '{print $2}'|grep -v label|sort > /tmp/vpsvultr 
+#/usr/local/bin/vucli serverlist -k $api_key | grep -oiE '([0-9]{1,3}\.){3}[0-9]{1,3}' | grep -v 255 | awk '{system("host "$0)}' | grep "name pointer" | cut -d " " -f 5 > /tmp/vpsvultr
+
+cat /etc/hosts |  grep -E '^([0-9]{1,3}\.){3}[0-9]{1,3}' | tr -s " " > /tmp/hosts
+
+rm /tmp/vpsvultr
+
+/usr/local/bin/vucli serverlist -k $api_key | grep -oiE '([0-9]{1,3}\.){3}[0-9]{1,3}' | awk '{system("grep " $0 " /tmp/hosts >> /tmp/vpsvultr")}'
+
+cat /tmp/vpsvultr | cut -d ' ' -f 2 | cat -b
 echo ""
 printf '%s ' 'vps number?'
 read -${BASH_VERSION+e}r vps
 vps2=$(sed -n $vps"p" /tmp/vpsvultr|awk '{print $1}')
 echo $vps2
-ssh root@$vps2 -p $port_ssh
+printf '%s ' 'user?'
+read -${BASH_VERSION+e}r user
+ssh $user@$vps2 -p $port_ssh
 exit 0
